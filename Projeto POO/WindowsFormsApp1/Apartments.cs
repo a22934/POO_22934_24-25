408adb21e389b1fa23d6885bc0488aa92a959d10
Paulo Jorge Lima Da Costa <a22934@alunos.ipca.pt>
2024-10-26 18:15:12 +0100

    classe apartamentos

---
+++ Projeto POO/WindowsFormsApp1/Apartments.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClientManagement_OOP
+{
+    public class Apartment
+    {
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public string Typology { get; set; } // Tipologia (T1, T2, T3)
+        public string PropertyType { get; set; } // Tipo imóvel (Moradia ou Apartamento)
+        public string AdditionalFeatures { get; set; } // Características adicionais
+
+        // Construtor para inicializar um apartamento
+        public Apartment(string location, string name, string typology, string propertyType, string additionalFeatures)
+        {
+            Location = location;
+            Name = name;
+            Typology = typology;
+            PropertyType = propertyType;
+            AdditionalFeatures = additionalFeatures;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Name}, Localização: {Location}, Tipologia: {Typology}, Tipo de Imóvel: {PropertyType}, Características Adicionais: {AdditionalFeatures}";
+        }
+    }
+}
